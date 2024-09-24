5d26973561c58b557523471a105d70ddeddba520
Tom Groenwoldt <tom.groenwoldt@gmail.com>
2024-09-24 21:48:24 +0200

    Move from new-keyglide to keyglide

--- client/src/config/audio.rs
+++ client/src/config/audio.rs
@@ -14,14 +14,14 @@ impl Audio {
         if let Some(ref reconnected) = self.reconnected {
             let path = Path::new(reconnected);
             if !path.exists() {
-                // TODO: Change this error when working on https://github.com/tomgroenwoldt/new-keyglide/issues/25.
+                // TODO: Change this error when working on https://github.com/tomgroenwoldt/keyglide/issues/25.
                 return Err(anyhow!("File {} does not exist...", reconnected));
             }
             let file_extension = path
                 .extension()
                 .expect("Path should have a file extension.");
             if !file_extension.eq("mp3") {
-                // TODO: Change this error when working on https://github.com/tomgroenwoldt/new-keyglide/issues/25.
+                // TODO: Change this error when working on https://github.com/tomgroenwoldt/keyglide/issues/25.
                 return Err(anyhow!("File {} is not MP3...", reconnected));
             }
         }
